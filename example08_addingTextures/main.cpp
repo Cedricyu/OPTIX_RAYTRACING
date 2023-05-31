33c76f779623ba3a0dfacb01b6b4c5a62c217684
Cedricyu <86541569+Cedricyu@users.noreply.github.com>
2023-05-31 23:29:52 +0800

    add movable window

--- example08_addingTextures/main.cpp
+++ example08_addingTextures/main.cpp
@@ -19,129 +19,180 @@
 // our helper library for window handling
 #include "glfWindow/GLFWindow.h"
 #include <GL/gl.h>
+#include <stdio.h>
+#include <string.h>
+#include <io.h>
+#include <direct.h>
+
+#define sizeOfModelList 20
 
 /*! \namespace osc - Optix Siggraph Course */
 namespace osc {
 
-  struct SampleWindow : public GLFCameraWindow
-  {
-    SampleWindow(const std::string &title,
-                 const Model *model,
-                 const Camera &camera,
-                 const float worldScale)
-      : GLFCameraWindow(title,camera.from,camera.at,camera.up,worldScale),
-        sample(model)
+    struct SampleWindow : public GLFCameraWindow
     {
-      sample.setCamera(camera);
-    }
-    
-    virtual void render() override
+        SampleWindow(const std::string& title,
+            const Model* model,
+            const Camera& camera,
+            const float worldScale)
+            : GLFCameraWindow(title, camera.from, camera.at, camera.up, worldScale),
+            sample(model)
+        {
+            sample.setCamera(camera);
+        }
+
+        virtual void render() override
+        {
+            if (cameraFrame.modified) {
+                sample.setCamera(Camera{ cameraFrame.get_from(),
+                                         cameraFrame.get_at(),
+                                         cameraFrame.get_up() });
+                cameraFrame.modified = false;
+            }
+            sample.render();
+        }
+
+        virtual void draw() override
+        {
+            sample.downloadPixels(pixels.data());
+            if (fbTexture == 0)
+                glGenTextures(1, &fbTexture);
+
+            glBindTexture(GL_TEXTURE_2D, fbTexture);
+            GLenum texFormat = GL_RGBA;
+            GLenum texelType = GL_UNSIGNED_BYTE;
+            glTexImage2D(GL_TEXTURE_2D, 0, texFormat, fbSize.x, fbSize.y, 0, GL_RGBA,
+                texelType, pixels.data());
+
+            glDisable(GL_LIGHTING);
+            glColor3f(1, 1, 1);
+
+            glMatrixMode(GL_MODELVIEW);
+            glLoadIdentity();
+
+            glEnable(GL_TEXTURE_2D);
+            glBindTexture(GL_TEXTURE_2D, fbTexture);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+            glDisable(GL_DEPTH_TEST);
+
+            glViewport(0, 0, fbSize.x, fbSize.y);
+
+            glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+            glOrtho(0.f, (float)fbSize.x, 0.f, (float)fbSize.y, -1.f, 1.f);
+
+            glBegin(GL_QUADS);
+            {
+                glTexCoord2f(0.f, 0.f);
+                glVertex3f(0.f, 0.f, 0.f);
+
+                glTexCoord2f(0.f, 1.f);
+                glVertex3f(0.f, (float)fbSize.y, 0.f);
+
+                glTexCoord2f(1.f, 1.f);
+                glVertex3f((float)fbSize.x, (float)fbSize.y, 0.f);
+
+                glTexCoord2f(1.f, 0.f);
+                glVertex3f((float)fbSize.x, 0.f, 0.f);
+            }
+            glEnd();
+        }
+
+        virtual void resize(const vec2i& newSize)
+        {
+            fbSize = newSize;
+            sample.resize(newSize);
+            pixels.resize(newSize.x * newSize.y);
+        }
+
+        vec2i                 fbSize;
+        GLuint                fbTexture{ 0 };
+        SampleRenderer        sample;
+        std::vector<uint32_t> pixels;
+    };
+
+    /*! main entry point to this example - initially optix, print hello
+      world, then exit */
+    extern "C" int main(int ac, char** av)
     {
-      if (cameraFrame.modified) {
-        sample.setCamera(Camera{ cameraFrame.get_from(),
-                                 cameraFrame.get_at(),
-                                 cameraFrame.get_up() });
-        cameraFrame.modified = false;
-      }
-      sample.render();
-    }
-    
-    virtual void draw() override
-    {
-      sample.downloadPixels(pixels.data());
-      if (fbTexture == 0)
-        glGenTextures(1, &fbTexture);
-      
-      glBindTexture(GL_TEXTURE_2D, fbTexture);
-      GLenum texFormat = GL_RGBA;
-      GLenum texelType = GL_UNSIGNED_BYTE;
-      glTexImage2D(GL_TEXTURE_2D, 0, texFormat, fbSize.x, fbSize.y, 0, GL_RGBA,
-                   texelType, pixels.data());
 
-      glDisable(GL_LIGHTING);
-      glColor3f(1, 1, 1);
+        const char* definePath = "C:\\Users\\derrick629\\Desktop\\OPTIX_RAYTRACING-main\\OPTIX_RAYTRACING-main\\models";
+        const char* extenName = "*.*";
 
-      glMatrixMode(GL_MODELVIEW);
-      glLoadIdentity();
+        char* curPath;
+        curPath = getcwd(NULL, 0);
+        // printf("%s\n", curPath);
 
-      glEnable(GL_TEXTURE_2D);
-      glBindTexture(GL_TEXTURE_2D, fbTexture);
-      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-      
-      glDisable(GL_DEPTH_TEST);
+        // get models
+        char buff[1024];
+        _chdir(definePath);
+        unsigned long long hFile;
+        struct _finddata_t fileName;
+        hFile = _findfirst(extenName, &fileName);
+        int count = 0;
+        char** modelList;
+        modelList = (char**)malloc(sizeOfModelList * sizeof(char*));
 
-      glViewport(0, 0, fbSize.x, fbSize.y);
+        do {
+            // if the path isn't "." or ".."
+            if (!strstr(fileName.name, ".")) {
+                int lenOfName = strlen(fileName.name);
+                modelList[count] = (char*)malloc((lenOfName + 1) * sizeof(char));
+                if (modelList[count] != NULL) {
+                    strncpy(modelList[count], fileName.name, lenOfName);
+                    modelList[count][lenOfName] = '\0';
+                    count++;
+                }
+            }
+        } while (hFile != -1 && _findnext(hFile, &fileName) == 0);
+        _findclose(hFile);
+        _chdir(curPath);
 
-      glMatrixMode(GL_PROJECTION);
-      glLoadIdentity();
-      glOrtho(0.f, (float)fbSize.x, 0.f, (float)fbSize.y, -1.f, 1.f);
 
-      glBegin(GL_QUADS);
-      {
-        glTexCoord2f(0.f, 0.f);
-        glVertex3f(0.f, 0.f, 0.f);
-      
-        glTexCoord2f(0.f, 1.f);
-        glVertex3f(0.f, (float)fbSize.y, 0.f);
-      
-        glTexCoord2f(1.f, 1.f);
-        glVertex3f((float)fbSize.x, (float)fbSize.y, 0.f);
-      
-        glTexCoord2f(1.f, 0.f);
-        glVertex3f((float)fbSize.x, 0.f, 0.f);
-      }
-      glEnd();
-    }
-    
-    virtual void resize(const vec2i &newSize) 
-    {
-      fbSize = newSize;
-      sample.resize(newSize);
-      pixels.resize(newSize.x*newSize.y);
-    }
+        for (int i = 0; i < count; i++) {
+            printf("(%d) %s \n", i, modelList[i]);
+        }
 
-    vec2i                 fbSize;
-    GLuint                fbTexture {0};
-    SampleRenderer        sample;
-    std::vector<uint32_t> pixels;
-  };
-  
-  
-  /*! main entry point to this example - initially optix, print hello
-    world, then exit */
-  extern "C" int main(int ac, char **av)
-  {
-    try {
-      Model *model = loadOBJ(
-#ifdef _WIN32
-      // on windows, visual studio creates _two_ levels of build dir
-      // (x86/Release)
-      "../../models/sponza.obj"
-#else
-      // on linux, common practice is to have ONE level of build dir
-      // (say, <project>/build/)...
-      "../models/sponza.obj"
-#endif
-                             );
-      Camera camera = { /*from*/vec3f(-1293.07f, 154.681f, -0.7304f),
-                        /* at */model->bounds.center()-vec3f(0,400,0),
-                        /* up */vec3f(0.f,1.f,0.f) };
-      // something approximating the scale of the world, so the
-      // camera knows how much to move for any given user interaction:
-      const float worldScale = length(model->bounds.span());
+        int option;
+        printf("Enter the number of the model you wanna play with: ");
+        scanf("%d", &option);
 
-      SampleWindow *window = new SampleWindow("Optix 7 Course Example",
-                                              model,camera,worldScale);
-      window->run();
-      
-    } catch (std::runtime_error& e) {
-      std::cout << GDT_TERMINAL_RED << "FATAL ERROR: " << e.what()
+        char modelPath[] = "../../models/";
+        char* modelName = NULL;
+        int lenOfName = strlen(modelList[option]);
+        modelName = (char*)malloc((strlen(modelList[option]) * 2 + 6) * sizeof(char));
+        strcpy(modelName, modelList[option]);
+        strcat(modelName, "/");
+        strcat(modelName, modelList[option]);
+        strcat(modelName, ".obj");
+        strcat(modelName, "\0");
+        strcat(modelPath, modelName);
+
+        try {
+            // char modelPath[] = "../../models/sponza/sponza.obj";
+            // Model *model = loadOBJ("../../models/sponza/sponza.obj");
+            Model* model = loadOBJ(modelPath);
+            Camera camera = { /*from*/vec3f(-1293.07f, 154.681f, -0.7304f),
+                /* at */model->bounds.center() - vec3f(0,400,0),
+                /* up */vec3f(0.f,1.f,0.f) };
+            // something approximating the scale of the world, so the
+            // camera knows how much to move for any given user interaction:
+            const float worldScale = length(model->bounds.span());
+
+            SampleWindow* window = new SampleWindow("Optix 7 Course Example",
+                model, camera, worldScale);
+            window->run();
+
+        }
+        catch (std::runtime_error& e) {
+            std::cout << GDT_TERMINAL_RED << "FATAL ERROR: " << e.what()
                 << GDT_TERMINAL_DEFAULT << std::endl;
-	  std::cout << "Did you forget to copy sponza.obj and sponza.mtl into your optix7course/models directory?" << std::endl;
-	  exit(1);
+            std::cout << "Did you forget to copy sponza.obj and sponza.mtl into your optix7course/models directory?" << std::endl;
+            exit(1);
+        }
+        return 0;
     }
-    return 0;
-  }
-  
+
 } // ::osc
